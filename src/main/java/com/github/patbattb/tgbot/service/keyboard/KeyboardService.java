352dbc1b48ad68b5db95561345d51f688c1e8d25
Patrick Bates <patbat.tb@gmail.com>
2023-09-18 21:52:24 +0300

    feature: InfoCommand добавляет ReplyKeyBoard

--- src/main/java/com/github/patbattb/tgbot/service/keyboard/KeyboardService.java
+++ src/main/java/com/github/patbattb/tgbot/service/keyboard/KeyboardService.java
@@ -2,7 +2,10 @@ package com.github.patbattb.tgbot.service.keyboard;
 
 import lombok.experimental.UtilityClass;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
 import java.util.List;
 
@@ -17,5 +20,19 @@ public class KeyboardService {
         buttonNo.setCallbackData(mask + "_NO");
         markup.setKeyboard(List.of(List.of(buttonYes, buttonNo)));
         return markup;
+    }
+
+    /* replyKeyBoard добавляется к sendMessage (Что бы удалить ее у клиента - нужно отредактировать сообщение),
+    либо что бы пользователь удалил сообщение у себя.
+     */
+    public ReplyKeyboardMarkup getDefaultKeyBoard() {
+        var markup = new ReplyKeyboardMarkup();
+        var keyboard = List.of(
+                new KeyboardRow(
+                        List.of(new KeyboardButton("Yes"), new KeyboardButton("No"))));
+        markup.setKeyboard(keyboard);
+        markup.setResizeKeyboard(true);
+        markup.setOneTimeKeyboard(true);
+        return  markup;
     }
 }
--- src/main/java/com/github/patbattb/tgbot/service/message/command/InfoCommand.java
+++ src/main/java/com/github/patbattb/tgbot/service/message/command/InfoCommand.java
@@ -1,8 +1,17 @@
 package com.github.patbattb.tgbot.service.message.command;
 
 import com.github.patbattb.tgbot.container.MethodContainer;
+import com.github.patbattb.tgbot.service.keyboard.KeyboardService;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.commands.DeleteMyCommands;
+import org.telegram.telegrambots.meta.api.methods.menubutton.SetChatMenuButton;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeChat;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
+
+import java.util.Collections;
 
 @Service
 public class InfoCommand implements Command {
@@ -10,6 +19,8 @@ public class InfoCommand implements Command {
     public void execute(MethodContainer methodContainer) {
         var chatId = methodContainer.getUpdate().getMessage().getChatId().toString();
         var message = "информационная команда... пока тут пусто";
-        methodContainer.getMethods().add(new SendMessage(chatId, message));
+        var sendMessage = new SendMessage(chatId, message);
+        sendMessage.setReplyMarkup(KeyboardService.getDefaultKeyBoard());
+        methodContainer.getMethods().add(sendMessage);
     }
 }
